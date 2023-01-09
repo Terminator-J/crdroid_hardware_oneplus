Terminator-J/crdroid_hardware_oneplus-old

DeviceExtras: Adapt for crDroid
Change namespace, tidy some whitespace, etc.

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.oneplus.DeviceExtras;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.main,
+                    DeviceExtrasActivity.class.getName(),
+                    R.drawable.ic_settings_device),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.lineageos.oneplus.DeviceExtras";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/Services/AutoHBMService.java
@@ -1,0 +1,134 @@
+package org.lineageos.oneplus.DeviceExtras.services;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.oneplus.DeviceExtras.DeviceExtras;
+import org.lineageos.oneplus.DeviceExtras.FileUtils;
+import org.lineageos.oneplus.DeviceExtras.R;
+
+public class AutoHBMService extends Service {
+    private static final int HBM_NODE = R.string.node_auto_hbm_service;
+
+    private static boolean mAutoHBMActive = false;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
+
+    public void activateLightSensorRead() {
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void deactivateLightSensorRead() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        mAutoHBMActive = false;
+        enableHBM(false);
+    }
+
+    private String getFile() {
+        String file = getApplicationContext().getString(HBM_NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    private void enableHBM(boolean enable) {
+        if (enable) {
+            FileUtils.writeValue(getFile(), "5");
+        } else {
+            FileUtils.writeValue(getFile(), "0");
+        }
+    }
+
+    private boolean isCurrentlyEnabled() {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            KeyguardManager km =
+                    (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
+            boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
+            float threshold = Float.parseFloat(mSharedPrefs.getString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, "20000"));
+            if (lux > threshold) {
+                if ((!mAutoHBMActive | !isCurrentlyEnabled()) && !keyguardShowing) {
+                    mAutoHBMActive = true;
+                    enableHBM(true);
+                }
+            }
+            if (lux < threshold) {
+                if (mAutoHBMActive) {
+                    mAutoHBMActive = false;
+                    enableHBM(false);
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

Terminator-J/crdroid_hardware_oneplus

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/ConfigPanelSearchIndexablesProvider.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.oneplus.DeviceExtras;
+
+
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.provider.SearchIndexableResource;
+import android.provider.SearchIndexablesProvider;
+
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_CLASS_NAME;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_ICON_RESID;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_ACTION;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RANK;
+import static android.provider.SearchIndexablesContract.COLUMN_INDEX_XML_RES_RESID;
+import static android.provider.SearchIndexablesContract.INDEXABLES_RAW_COLUMNS;
+import static android.provider.SearchIndexablesContract.INDEXABLES_XML_RES_COLUMNS;
+import static android.provider.SearchIndexablesContract.NON_INDEXABLES_KEYS_COLUMNS;
+
+public class ConfigPanelSearchIndexablesProvider extends SearchIndexablesProvider {
+    private static final String TAG = "ConfigPanelSearchIndexablesProvider";
+
+    public static final int SEARCH_IDX_BUTTON_PANEL = 0;
+    public static final int SEARCH_IDX_GESTURE_PANEL = 1;
+    public static final int SEARCH_IDX_OCLICK_PANEL = 2;
+
+    private static SearchIndexableResource[] INDEXABLE_RES = new SearchIndexableResource[]{
+            new SearchIndexableResource(1, R.xml.main,
+                    DeviceExtrasActivity.class.getName(),
+                    R.drawable.ic_device_extras),
+    };
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public Cursor queryXmlResources(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_XML_RES_COLUMNS);
+        return cursor;
+    }
+
+    private static Object[] generateResourceRef(SearchIndexableResource sir) {
+        Object[] ref = new Object[7];
+        ref[COLUMN_INDEX_XML_RES_RANK] = sir.rank;
+        ref[COLUMN_INDEX_XML_RES_RESID] = sir.xmlResId;
+        ref[COLUMN_INDEX_XML_RES_CLASS_NAME] = null;
+        ref[COLUMN_INDEX_XML_RES_ICON_RESID] = sir.iconResId;
+        ref[COLUMN_INDEX_XML_RES_INTENT_ACTION] = "com.android.settings.action.EXTRA_SETTINGS";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_PACKAGE] = "org.lineageos.oneplus.DeviceExtras";
+        ref[COLUMN_INDEX_XML_RES_INTENT_TARGET_CLASS] = sir.className;
+        return ref;
+    }
+
+    @Override
+    public Cursor queryRawData(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(INDEXABLES_RAW_COLUMNS);
+        return cursor;
+    }
+
+    @Override
+    public Cursor queryNonIndexableKeys(String[] projection) {
+        MatrixCursor cursor = new MatrixCursor(NON_INDEXABLES_KEYS_COLUMNS);
+        return cursor;
+    }
+}

DeviceExtras/src/org/lineageos/oneplus/DeviceExtras/Services/AutoHBMService.java
@@ -1,0 +1,134 @@
+package org.lineageos.oneplus.DeviceExtras.services;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.oneplus.DeviceExtras.DeviceExtras;
+import org.lineageos.oneplus.DeviceExtras.FileUtils;
+import org.lineageos.oneplus.DeviceExtras.R;
+
+public class AutoHBMService extends Service {
+    private static final int HBM_NODE = R.string.node_hbm_mode_switch;
+
+    private static boolean mAutoHBMActive = false;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
+
+    public void activateLightSensorRead() {
+        mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void deactivateLightSensorRead() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        mAutoHBMActive = false;
+        enableHBM(false);
+    }
+
+    private String getFile() {
+        String file = getApplicationContext().getString(HBM_NODE);
+        if (FileUtils.fileWritable(file)) {
+            return file;
+        }
+        return null;
+    }
+
+    private void enableHBM(boolean enable) {
+        if (enable) {
+            FileUtils.writeValue(getFile(), "5");
+        } else {
+            FileUtils.writeValue(getFile(), "0");
+        }
+    }
+
+    private boolean isCurrentlyEnabled() {
+        return FileUtils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    SensorEventListener mSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            KeyguardManager km =
+                    (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
+            boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
+            float threshold = Float.parseFloat(mSharedPrefs.getString(DeviceExtras.KEY_AUTO_HBM_THRESHOLD, "20000"));
+            if (lux > threshold) {
+                if ((!mAutoHBMActive | !isCurrentlyEnabled()) && !keyguardShowing) {
+                    mAutoHBMActive = true;
+                    enableHBM(true);
+                }
+            }
+            if (lux < threshold) {
+                if (mAutoHBMActive) {
+                    mAutoHBMActive = false;
+                    enableHBM(false);
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}